f024bd11a83ec01b49c6b2ae63e6cfc9bfef17ad
Matthieu Cardinaux <matthieu.cardinaux@gmail.com>
2018-10-10 11:03:25 +0200

    Bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-taskq',
-    version='0.0.5',
+    version='0.0.6',
     description='yet another task queue',
     author='ipso sante',
     author_email='contact@ipsosante.fr',
