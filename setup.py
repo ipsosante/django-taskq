71513d839fd44b2b0ca224234bdf0018104c97fb
Guillaume Algis <guillaume.algis@ipsosante.fr>
2018-11-22 11:48:40 +0100

    Improve package setup

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='django-taskq',
     version='0.1.0',
-    description='yet another task queue',
+    description='Yet another task queue',
     author='ipso sante',
     author_email='contact@ipsosante.fr',
     url='https://github.com/ipsosante/django-taskq',
     long_description='',
-    packages=find_packages(),
+    packages=[
+        'taskq',
+    ],
     zip_safe=False,
     install_requires=[
-        'django',
-        'croniter',
+        'django >= 1.9, < 2.0',
+        'croniter >= 0.3, <=0.4',
     ],
 )
