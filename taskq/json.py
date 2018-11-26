e4b1c7cfd36eee9e18ac3631c3b212c420c439d8
Guillaume Algis <guillaume.algis@ipsosante.fr>
2018-11-26 17:13:34 +0100

    Add tests for json encoder and decoder

--- taskq/json.py
+++ taskq/json.py
@@ -6,53 +6,49 @@ class JSONEncoder(json.JSONEncoder):
 
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
-            return {
-                '__type__': 'datetime',
-                'year': obj.year,
-                'month': obj.month,
-                'day': obj.day,
-                'hour': obj.hour,
-                'minute': obj.minute,
-                'second': obj.second,
-                'microsecond': obj.microsecond,
-            }
+            return self.default_datetime(obj)
 
-        elif isinstance(obj, datetime.timedelta):
-            return {
-                '__type__': 'timedelta',
-                'days': obj.days,
-                'seconds': obj.seconds,
-                'microseconds': obj.microseconds,
-            }
+        if isinstance(obj, datetime.timedelta):
+            return self.default_timedelta(obj)
 
-        else:
-            return json.JSONEncoder.default(self, obj)
+        return super().default(obj)
+
+    def default_datetime(self, obj):
+        return {
+            '__type__': 'datetime',
+            'year': obj.year,
+            'month': obj.month,
+            'day': obj.day,
+            'hour': obj.hour,
+            'minute': obj.minute,
+            'second': obj.second,
+            'microsecond': obj.microsecond,
+        }
+
+    def default_timedelta(self, obj):
+        return {
+            '__type__': 'timedelta',
+            'days': obj.days,
+            'seconds': obj.seconds,
+            'microseconds': obj.microseconds,
+        }
 
 
 class JSONDecoder(json.JSONDecoder):
 
     def __init__(self):
-        json.JSONDecoder.__init__(self, object_hook=self.dict_to_object)
+        super().__init__(object_hook=self.dict_to_object)
 
-    def dict_to_object(self, d):
-        if '__type__' not in d:
-            return d
+    def dict_to_object(self, obj_dict):
+        if '__type__' not in obj_dict:
+            return obj_dict
 
-        obj_type = d.pop('__type__')
+        obj_type = obj_dict.pop('__type__')
         if obj_type == 'datetime':
-            return datetime.datetime(**d)
-        elif obj_type == 'timedelta':
-            return datetime.timedelta(**d)
-        else:
-            d['__type__'] = obj_type
-            return d
+            return datetime.datetime(**obj_dict)
+        if obj_type == 'timedelta':
+            return datetime.timedelta(**obj_dict)
 
-# a = {
-#     'date': datetime.datetime.now()
-# }
-
-# dump = json.dumps(a, cls=JSONEncoder)
-
-# print(dump)
-
-# print(json.loads(dump, cls=JSONDecoder))
+        # Put __type__ back in the object dict
+        obj_dict['__type__'] = obj_type
+        return obj_dict
