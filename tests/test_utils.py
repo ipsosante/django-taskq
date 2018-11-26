e14db9d81c538bd63ed80a2b9412602276f0575f
Guillaume Algis <guillaume.algis@ipsosante.fr>
2018-11-26 19:17:08 +0100

    Add test for utils.task_from_scheduled_task

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,8 @@ import datetime
 
 from django.test import TestCase
 
-from taskq.utils import delay_timedelta
+from taskq.utils import delay_timedelta, task_from_scheduled_task
+from taskq.scheduler import ScheduledTask
 
 
 class UtilsDelayTimedeltaTestCase(TestCase):
@@ -31,3 +32,27 @@ class UtilsDelayTimedeltaTestCase(TestCase):
             year=2000, month=4, day=20
         ))
         self.assertRaises(TypeError, delay_timedelta, [2, 45])
+
+
+class UtilsTaskFromScheduledTaskTestCase(TestCase):
+
+    def test_can_create_task_from_scheduled_task(self):
+        """task_from_scheduled_task creates a new Task from a ScheduledTask."""
+        args = {
+            'flour': 300,
+            'pumpkin': True,
+        }
+        scheduled_task = ScheduledTask(
+            name="Cooking pie", task="kitchen.chef.cook_pie", cron="0 19 * * *", args=args, max_retries=1,
+            retry_delay=22, retry_backoff=True, retry_backoff_factor=2
+        )
+
+        task = task_from_scheduled_task(scheduled_task)
+        self.assertIsNotNone(task)
+        self.assertEqual(task.name, "Cooking pie")
+        self.assertEqual(task.function_name, "kitchen.chef.cook_pie")
+        self.assertEqual(task.function_args, '{"flour": 300, "pumpkin": true}')
+        self.assertEqual(task.max_retries, 1)
+        self.assertEqual(task.retry_delay, datetime.timedelta(seconds=22))
+        self.assertEqual(task.retry_backoff, True)
+        self.assertEqual(task.retry_backoff_factor, 2)
