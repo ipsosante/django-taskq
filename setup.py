14a4769f0985236573274ddbd0394b17209c54a7
Youenn <youenn.boussard@ipsosante.fr>
2019-09-16 15:10:29 +0200

    update dependency for django in setup.py and requirement.txt.

--- setup.py
+++ setup.py
@@ -3,16 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-taskq',
-    version='0.3.0',
-    description='Yet another task queue',
-    author='ipso sante',
-    author_email='contact@ipsosante.fr',
-    url='https://github.com/ipsosante/django-taskq',
-    long_description='',
-    packages=find_packages(exclude=['tests']),
+    name="django-taskq",
+    version="0.3.0",
+    description="Yet another task queue",
+    author="ipso sante",
+    author_email="contact@ipsosante.fr",
+    url="https://github.com/ipsosante/django-taskq",
+    long_description="",
+    packages=find_packages(exclude=["tests"]),
     zip_safe=False,
-    install_requires=[
-        'croniter >= 0.3, <=0.4',
-    ],
+    install_requires=["django>=1.9, <3.0", "croniter >= 0.3, <=0.4"],
 )
