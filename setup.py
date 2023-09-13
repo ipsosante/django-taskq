7c6ffde48082f2163872c81818e09a9c6c265596
youenn <youenn.boussard@ipsosante.fr>
2023-09-13 18:46:58 +0200

    Bump version: 0.5.2 → 0.6.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-taskq",
-    version="0.5.2",
+    version="0.6.0",
     description="Yet another task queue",
     author="ipso sante",
     author_email="contact@ipsosante.fr",
